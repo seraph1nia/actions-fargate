44c2e789a4652777f3afb2bf0f1cdbd715fa9af9
Bart Tange <barttange@gmail.com>
2021-10-27 19:30:06 +0200

    formatted

--- terraform/service1.tf
+++ terraform/service1.tf
@@ -1,50 +1,52 @@
 resource "aws_ecs_task_definition" "main" {
-  family = "${var.name}-service-${var.environment}"
+  family                   = "${var.name}-service-${var.environment}"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = 30
   memory                   = 50
   # dit geeft fargate rechten nodig
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  task_role_arn            = aws_iam_role.ecs_task_role.arn
-  container_definitions = jsonencode([{
-   name        = "${var.name}-container-${var.environment}"
-   image       = var.image
-   essential   = true
-   environment = var.environment
-   portMappings = [{
-     protocol      = "tcp"
-     containerPort = 80
-     hostPort      = 80
-   }]
-  }])
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
+  container_definitions = jsonencode([
+    {
+      name        = "${var.name}-container-${var.environment}"
+      image       = var.image
+      essential   = true
+      environment = var.environment
+      portMappings = [{
+        protocol      = "tcp"
+        containerPort = 80
+        hostPort      = 80
+      }]
+    }
+  ])
 }
 
 resource "aws_ecs_service" "main" {
- name                               = "${var.name}-service-${var.environment}"
- cluster                            = aws_ecs_cluster.main.id
- task_definition                    = aws_ecs_task_definition.main.arn
- desired_count                      = 2
- deployment_minimum_healthy_percent = 50
- deployment_maximum_percent         = 200
- launch_type                        = "FARGATE"
- scheduling_strategy                = "REPLICA"
- 
- network_configuration {
-   security_groups  = [aws_security_group.ecs_tasks.id]
-   subnets          = [aws_subnet.private.id]
-   assign_public_ip = false
- }
- 
- load_balancer {
-   target_group_arn = aws_alb_target_group.main.arn
-   container_name   = "${var.name}-container-${var.environment}"
-   container_port   = 80
- }
- # wat is hier ook alweer de bedoeling van?
- lifecycle {
-   ignore_changes = [task_definition, desired_count]
- }
+  name                               = "${var.name}-service-${var.environment}"
+  cluster                            = aws_ecs_cluster.main.id
+  task_definition                    = aws_ecs_task_definition.main.arn
+  desired_count                      = 2
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+  launch_type                        = "FARGATE"
+  scheduling_strategy                = "REPLICA"
+
+  network_configuration {
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    subnets          = [aws_subnet.private.id]
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.main.arn
+    container_name   = "${var.name}-container-${var.environment}"
+    container_port   = 80
+  }
+  # wat is hier ook alweer de bedoeling van?
+  lifecycle {
+    ignore_changes = [task_definition, desired_count]
+  }
 }
 
 resource "aws_lb" "main" {
@@ -53,25 +55,25 @@ resource "aws_lb" "main" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb.id]
   subnets            = aws_subnet.public.*.id # gebruik gewoon alle uit dit project
- 
+
   enable_deletion_protection = false
 }
- 
+
 resource "aws_alb_target_group" "main" {
   name        = "${var.name}-tg-${var.environment}"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.main.id
   target_type = "ip"
- 
+
   health_check {
-   healthy_threshold   = "3"
-   interval            = "30"
-   protocol            = "HTTP"
-   matcher             = "200"
-   timeout             = "3"
-   path                = "/" # nog geen healthcheck, gewoon de rootpage
-   unhealthy_threshold = "2"
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/" # nog geen healthcheck, gewoon de rootpage
+    unhealthy_threshold = "2"
   }
 }
 
@@ -79,18 +81,18 @@ resource "aws_alb_listener" "http" {
   load_balancer_arn = aws_lb.main.id
   port              = 80
   protocol          = "HTTP"
- 
+
   # nog even geen redirect, want geen certificaat.
   # default_action {
   #  type = "redirect"
- 
+
   #  redirect {
   #    port        = 443
   #    protocol    = "HTTPS"
   #    status_code = "HTTP_301"
   #  }
   # }
-    default_action {
+  default_action {
     target_group_arn = aws_alb_target_group.main.id
     type             = "forward"
   }
@@ -102,10 +104,10 @@ resource "aws_alb_listener" "http" {
 #   load_balancer_arn = aws_lb.main.id
 #   port              = 443
 #   protocol          = "HTTPS"
- 
+
 #   ssl_policy        = "ELBSecurityPolicy-2016-08"
 #   certificate_arn   = var.alb_tls_cert_arn
- 
+
 #   default_action {
 #     target_group_arn = aws_alb_target_group.main.id
 #     type             = "forward"
@@ -128,29 +130,29 @@ resource "aws_appautoscaling_policy" "ecs_policy_memory" {
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
- 
+
   target_tracking_scaling_policy_configuration {
-   predefined_metric_specification {
-     predefined_metric_type = "ECSServiceAverageMemoryUtilization"
-   }
- 
-   target_value       = 80
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = 80
   }
 }
- 
+
 resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
   name               = "cpu-autoscaling"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.ecs_target.resource_id
   scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
   service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
- 
+
   target_tracking_scaling_policy_configuration {
-   predefined_metric_specification {
-     predefined_metric_type = "ECSServiceAverageCPUUtilization"
-   }
- 
-   target_value       = 60
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 60
   }
 }
 
@@ -158,29 +160,29 @@ resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
 resource "aws_security_group" "alb" {
   name   = "${var.name}-sg-alb-${var.environment}"
   vpc_id = aws_vpc.main.id
- 
+
   ingress {
-   protocol         = "tcp"
-   from_port        = 80
-   to_port          = 80
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
- 
+
   ingress {
-   protocol         = "tcp"
-   from_port        = 443
-   to_port          = 443
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
+    protocol         = "tcp"
+    from_port        = 443
+    to_port          = 443
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
- 
+
   egress {
-   protocol         = "-1"
-   from_port        = 0
-   to_port          = 0
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
 
@@ -188,20 +190,20 @@ resource "aws_security_group" "alb" {
 resource "aws_security_group" "ecs_tasks" {
   name   = "${var.name}-sg-task-${var.environment}"
   vpc_id = aws_vpc.main.id
- 
+
   ingress {
-   protocol         = "tcp"
-   from_port        = 80
-   to_port          = 80
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
+    protocol         = "tcp"
+    from_port        = 80
+    to_port          = 80
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
- 
+
   egress {
-   protocol         = "-1"
-   from_port        = 0
-   to_port          = 0
-   cidr_blocks      = ["0.0.0.0/0"]
-   ipv6_cidr_blocks = ["::/0"]
+    protocol         = "-1"
+    from_port        = 0
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
